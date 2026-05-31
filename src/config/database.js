e058101fc531d5620d863df5d95735a94ca799cd
mibnusofyann <muhammadibnusofyan003@gmail.com>
2026-05-31 19:15:45 +0700

    config databse for deployment

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,29 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+const getEnv = (...keys) => {
+  for (const key of keys) {
+    if (Object.prototype.hasOwnProperty.call(process.env, key)) {
+      return process.env[key];
+    }
+  }
+
+  return undefined;
+};
+
+const dbName = getEnv("DB_NAME", "MYSQLDATABASE");
+const dbUser = getEnv("DB_USER", "MYSQLUSER");
+const dbPassword = getEnv("DB_PASSWORD", "MYSQLPASSWORD");
+const dbHost = getEnv("DB_HOST", "MYSQLHOST");
+const dbPort = getEnv("DB_PORT", "MYSQLPORT");
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  dbName,
+  dbUser,
+  dbPassword,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: dbHost,
+    port: dbPort,
     dialect: "mysql",
     logging: false,
   },
