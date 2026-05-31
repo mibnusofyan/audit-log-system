1041131f9041513f5f0fd163c5691ab50a6e823d
mibnusofyann <muhammadibnusofyan003@gmail.com>
2026-05-31 19:06:03 +0700

    add route /users/privileged

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,11 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
-const { verifyToken } = require("../middleware/auth.middleware");
+const { verifyToken, isAdmin } = require("../middleware/auth.middleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/logout", verifyToken, authController.logout);
+
+router.post(
+  "/users/privileged",
+  verifyToken,
+  isAdmin,
+  authController.createPrivilegedUser,
+);
 
 // contoh protected route
 router.get("/profile", verifyToken, (req, res) => {
